95e62887f3b7184a6bbf9159fea7150b569e6dce
Sergey Glazyrin <sergey.glazyrin.dev@gmail.com>
2015-11-28 01:04:33 +0100

    middleware - another way to setup it

--- src/rit/core/db/_sessions.py
+++ src/rit/core/db/_sessions.py
@@ -4,12 +4,18 @@ from sqlalchemy.orm import sessionmaker
 from rit.app.conf import settings
 
 sessions = {}
-for alias in ('default', 'default_slave'):
-    engine = create_engine(
-        settings.DATABASES[alias],
-        echo=settings.DEBUG,
-        isolation_level="AUTOCOMMIT"
-    )
 
-    Session = sessionmaker(bind=engine, autocommit=True)
-    sessions[alias] = Session()
+
+def setup_database_sessions():
+    for alias in ('default', 'default_slave'):
+        engine = create_engine(
+            settings.DATABASES[alias],
+            echo=settings.DEBUG,
+            isolation_level="AUTOCOMMIT"
+        )
+
+        Session = sessionmaker(bind=engine, autocommit=True)
+        sessions[alias] = Session()
+
+if settings.SETUP_DATABASE_SESSIONS:
+    setup_database_sessions()
--- src/rit/core/web/wsgi.py
+++ src/rit/core/web/wsgi.py
@@ -5,15 +5,18 @@ from rit.core.web.middleware_debug import rit_http_error_middleware_factory
 
 from rit.core.web.urls import all_urls
 from rit.app.conf.configure_template import template_handler
+from rit.app.conf.settings import EXTRA_MIDDLEWARES
 
 options = {
     'render_template': template_handler
 }
+
+middlewares = EXTRA_MIDDLEWARES + [
+    rit_http_error_middleware_factory,
+    bootstrap_defaults(url_mapping=all_urls),
+    path_routing_middleware_factory,
+]
 application = WSGIApplication(
-    middleware=[
-        rit_http_error_middleware_factory,
-        bootstrap_defaults(url_mapping=all_urls),
-        path_routing_middleware_factory,
-    ],
+    middleware=middlewares,
     options=options
 )
